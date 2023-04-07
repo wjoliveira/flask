0047424cdb9d57459ee1b97df71283edc184fa1f
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2023-04-07 18:11:49 -0300

    Aula 17 - Utilizando a API TMDB

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import urllib.request, json
 
 app = Flask(__name__)
 
@@ -19,5 +20,18 @@ def sobre():
         if request.form.get("aluno") and request.form.get("nota"):
             registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
     return render_template("sobre.html", registros=registros)
+
+@app.route('/filmes')
+def filmes():
+    url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ea18c9592befcd0f9e9edd821813cbb6"
+    
+    resposta = urllib.request.urlopen(url)
+
+    dados = resposta.read()
+
+    jsondata = json.loads(dados)
+
+    return render_template("filmes.html", filmes=jsondata['results'])
+    #return jsondata['results']
 
 #http://127.0.0.1:5000 - endereço padrão

dcffb1ae542d13461a4bdba0b97ce48a7d107e6b
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2023-04-07 14:37:25 -0300

    Aula 6 - Integrando com HTML

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "Olá Mundo!"
+    return render_template("index.html")
 
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
 
 #http://127.0.0.1:5000 - endereço padrão

020303e81a31882a561e10cb4b0ddfb79a8089e5
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2023-04-07 15:51:25 -0300

    Alula 11 - Adicionando dados em dicionário

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 frutas = []
+registros = []
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -12,9 +13,12 @@ def index():
             frutas.append(request.form.get("fruta"))
     return render_template("index.html", frutas=frutas)
 
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    notas = {"Aluno0":5.0, "Aluno1":8.0, "Aluno2":7.0, "Aluno3":8.5}
-    return render_template("sobre.html", notas=notas)
+    # alunos = {"Aluno0":5.0, "Aluno1":8.0, "Aluno2":7.0, "Aluno3":8.5}
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
+    return render_template("sobre.html", registros=registros)
 
 #http://127.0.0.1:5000 - endereço padrão

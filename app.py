4788cf214518b0dc8907fcfc2cdb4729c3d5498c
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2023-04-07 15:17:46 -0300

    Aula 9 - Retornando valores de dicionário

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    frutas = ["Morango", "Uva", "Maçã", "Laranja"]
+    frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Pêra", "Melão", "Abacaxi"]
     return render_template("index.html", frutas=frutas)
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
+    notas = {"Aluno0":5.0, "Aluno1":8.0, "Aluno2":7.0, "Aluno3":8.5}
+    return render_template("sobre.html", notas=notas)
 
 #http://127.0.0.1:5000 - endereço padrão

05b360dde427c8bba2e6db7d6a9d8db06e674be6
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2023-04-08 11:15:12 -0300

    Aula 21 - Criando o Banco de Dados

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request
 import urllib.request, json
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.sqlite3"
+
+# initialize the app with the extension
+db.init_app(app)
+
 frutas = []
 registros = []
+
+class Cursos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    descricao = db.Column(db.String(100))
+    ch = db.Column(db.Integer)
+
+    def __init__(self, nome, descricao, ch):
+        self.nome = nome
+        self.descricao = descricao
+        self.ch = ch
+
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/index', methods=["GET", "POST"])
@@ -44,5 +65,9 @@ def filmes(propriedade):
     jsondata = json.loads(dados)
 
     return render_template("filmes.html", filmes=jsondata['results'])
+
+if __name__=="__main__":
+    db.create_all()
+    #app.run(debug=True)
 
 #http://127.0.0.1:5000 - endereço padrão
